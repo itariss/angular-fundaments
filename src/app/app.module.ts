9cc08d12e689aa448a783113b92dee7ac02bc14e
itariss <itariss@protonmail.com>
2022-11-22 19:38:16 -0300

    Creating project template and first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PrimeiroComponentComponent } from "./primeiro-componente/primeiro-component.component";
+import { SegundoComponenteComponent } from "./segundo-componente/segundo-componente.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		PrimeiroComponentComponent,
+		SegundoComponenteComponent
+	],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/primeiro-componente/primeiro-component.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-primeiro-component",
+	template: "<h2>Primeiro Componente</h2>",
+	styles: ["h2 {color: red;}"]
+})
+export class PrimeiroComponentComponent {}
