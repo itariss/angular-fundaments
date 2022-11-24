935bdf7cf1036ea50adea487a8b46a5c7cf061d5
itariss <itariss@protonmail.com>
2022-11-24 17:01:32 -0300

    Event binding to parent component

--- src/app/componente-filho/componente-filho.component.ts
+++ src/app/componente-filho/componente-filho.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
 	selector: "app-componente-filho",
@@ -8,5 +8,8 @@ import { Component, Input } from "@angular/core";
 export class ComponenteFilhoComponent {
 	@Input()
 	sobrenome: string = "";
+
+	@Output()
+	mostraNome = new EventEmitter();
 	nome: string = "";
 }
--- src/app/componente-pai/componente-pai.component.ts
+++ src/app/componente-pai/componente-pai.component.ts
@@ -7,4 +7,8 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ComponentePaiComponent {
 	sobrenome: string = "da Silva";
+
+	mostraNomeCompleto(nomeCompleto: Event): void {
+		alert(`O nome completo é: ${nomeCompleto}`);
+	}
 }
